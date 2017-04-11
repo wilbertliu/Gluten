b4863da8b28ecdd33171bc755ff21247a3019d18
Wilbert Liu <wilbertliu@gmail.com>
2017-04-11 14:15:33 +0700

    Add initial project

---
+++ Gluten/Gluten.h
@@ -0,0 +1,19 @@
+//
+//  Gluten.h
+//  Gluten
+//
+//  Created by Wilbert Liu on 4/11/17.
+//  Copyright © 2017 Wilbert Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gluten.
+FOUNDATION_EXPORT double GlutenVersionNumber;
+
+//! Project version string for Gluten.
+FOUNDATION_EXPORT const unsigned char GlutenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gluten/PublicHeader.h>
+
+
